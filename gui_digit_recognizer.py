5d23b1641efbaf629b27c9b1eec395bfe64eb2bc
vnpthuuquang <113606382+vnpthuuquang@users.noreply.github.com>
2022-10-28 22:43:40 +0700

    Update gui_digit_recognizer.py

--- gui_digit_recognizer.py
+++ gui_digit_recognizer.py
@@ -2,7 +2,6 @@ from keras.models import load_model
 from tkinter import *
 import tkinter as tk
 import win32gui
-
 from PIL import ImageGrab, ImageOps
 import numpy as np
 
@@ -11,9 +10,7 @@ model = load_model('mnist.h5')
 def predict_digit(img):
     img = img.resize((28,28))
     img = img.convert('L')
-    
     img = ImageOps.invert(img)
-    
     img = np.array(img)
     img = img.reshape(1,28,28,1)
     img = img/255.0
@@ -23,19 +20,15 @@ def predict_digit(img):
 class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-
         self.x = self.y = 0
-        
         self.canvas = tk.Canvas(self, width=300, height=300, bg = "white", cursor="cross")
         self.label = tk.Label(self, text="Draw..", font=("Helvetica", 48))
         self.classify_btn = tk.Button(self, text = "Recognise", command = self.classify_handwriting)   
         self.button_clear = tk.Button(self, text = "Clear", command = self.clear_all)
-       
         self.canvas.grid(row=0, column=0, pady=2, sticky=W, )
         self.label.grid(row=0, column=1,pady=2, padx=2)
         self.classify_btn.grid(row=1, column=1, pady=2, padx=2)
         self.button_clear.grid(row=1, column=0, pady=2)
-        
         self.canvas.bind("<B1-Motion>", self.draw_lines)
 
     def clear_all(self):
@@ -47,7 +40,6 @@ class App(tk.Tk):
         a,b,c,d = rect
         rect=(a+4,b+4,c-4,d-4)
         im = ImageGrab.grab(rect)
-
         digit, acc = predict_digit(im)
         self.label.configure(text= str(digit)+', '+ str(int(acc*100))+'%')
 
